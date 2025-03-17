fd8b578da35ece83fff4538dcc70eaae95280c75
mouad kaaib <elcosto02@gmail.com>
2025-03-17 13:28:26 +0000

    project initialization

---
+++ src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/TpSpringMvcSpringDataJpaThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_spring_mvc_spring_data_jpa_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringMvcSpringDataJpaThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringMvcSpringDataJpaThymeleafApplication.class, args);
+	}
+
+}
