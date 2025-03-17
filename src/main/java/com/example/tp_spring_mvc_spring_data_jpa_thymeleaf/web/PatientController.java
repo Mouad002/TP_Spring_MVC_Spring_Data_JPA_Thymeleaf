60bc23f1601c45db68d05d53e040f4fcc25fa2e7
mouad kaaib <elcosto02@gmail.com>
2025-03-17 15:33:24 +0000

    add pagination

--- src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/web/PatientController.java
+++ src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/web/PatientController.java
@@ -2,9 +2,12 @@ package com.example.tp_spring_mvc_spring_data_jpa_thymeleaf.web;
 
 import com.example.tp_spring_mvc_spring_data_jpa_thymeleaf.entities.Patient;
 import com.example.tp_spring_mvc_spring_data_jpa_thymeleaf.repositories.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,9 +19,11 @@ public class PatientController {
         this.patientRepository = patientRepository;
     }
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public String index(Model model, @RequestParam(value = "page", defaultValue = "0") int page ,@RequestParam(value = "size", defaultValue = "5") int size) {
+        Page<Patient> patients = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("patients", patients.getContent());
+        model.addAttribute("pages", new int[patients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "patients";
     }
 }
