765e9d1996f0f3af67461b4b84cc1b94843cb6d0
mouad kaaib <elcosto02@gmail.com>
2025-03-17 18:18:50 +0000

    add enhancement procedure | ending of part one

--- src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/repositories/PatientRepository.java
+++ src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/repositories/PatientRepository.java
@@ -1,9 +1,15 @@
 package com.example.tp_spring_mvc_spring_data_jpa_thymeleaf.repositories;
 
 import com.example.tp_spring_mvc_spring_data_jpa_thymeleaf.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-
+    public Page<Patient> findByNomContains(String nom, Pageable pageable);
+    @Query("select p from Patient p where p.nom like :x")
+    public Page<Patient> search(@Param("x") String nom, Pageable pageable);
 }
 
--- src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/web/PatientController.java
+++ src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/web/PatientController.java
@@ -4,6 +4,7 @@ import com.example.tp_spring_mvc_spring_data_jpa_thymeleaf.entities.Patient;
 import com.example.tp_spring_mvc_spring_data_jpa_thymeleaf.repositories.PatientRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +20,21 @@ public class PatientController {
         this.patientRepository = patientRepository;
     }
     @GetMapping("/index")
-    public String index(Model model, @RequestParam(value = "page", defaultValue = "0") int page ,@RequestParam(value = "size", defaultValue = "5") int size) {
-        Page<Patient> patients = patientRepository.findAll(PageRequest.of(page,size));
+    public String index(Model model,
+                        @RequestParam(value = "page", defaultValue = "0") int page,
+                        @RequestParam(value = "size", defaultValue = "5") int size,
+                        @RequestParam(value = "keyword", defaultValue = "") String keyword) {
+        Page<Patient> patients = patientRepository.findByNomContains(keyword, PageRequest.of(page,size));
         model.addAttribute("patients", patients.getContent());
         model.addAttribute("pages", new int[patients.getTotalPages()]);
         model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", keyword);
         return "patients";
+    }
+
+    @GetMapping("/delete")
+    public String delete(Model model, Long id, int page, String keyword) {
+        patientRepository.deleteById(id);
+        return "redirect:/index?page=" + page + "&keyword=" + keyword;
     }
 }
