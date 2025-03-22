10222f57059999e171ccd6fb4a92d1006c520748
mouad kaaib <elcosto02@gmail.com>
2025-03-22 23:48:38 +0000

    add the documentation of part two

--- src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/web/PatientController.java
+++ src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/web/PatientController.java
@@ -5,7 +5,6 @@ import com.example.tp_spring_mvc_spring_data_jpa_thymeleaf.repositories.PatientR
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
