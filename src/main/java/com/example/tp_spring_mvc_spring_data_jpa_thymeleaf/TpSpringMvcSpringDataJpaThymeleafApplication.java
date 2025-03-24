59e7253de6abe63911ef49ca9fdead7362f352f9
mouad kaaib <elcosto02@gmail.com>
2025-03-24 15:07:19 +0000

    add passwordEncoder to SecurityConfig

--- src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/TpSpringMvcSpringDataJpaThymeleafApplication.java
+++ src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/TpSpringMvcSpringDataJpaThymeleafApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -31,5 +34,10 @@ public class TpSpringMvcSpringDataJpaThymeleafApplication implements CommandLine
 
 //		patientRepository.save(p1);
 //		patientRepository.save(p2);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
--- src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/security/SecurityConfig.java
+++ src/main/java/com/example/tp_spring_mvc_spring_data_jpa_thymeleaf/security/SecurityConfig.java
@@ -1,19 +1,35 @@
 package com.example.tp_spring_mvc_spring_data_jpa_thymeleaf.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.formLogin(Customizer.withDefaults());
         httpSecurity.authorizeHttpRequests(ar -> ar.anyRequest().authenticated());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("USER","ADMIN").build()
+        );
     }
 }
